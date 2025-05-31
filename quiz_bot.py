9bd1ed8ffd32c0232ac693916fc00c5cb5224875
Zealina Hudson <zealinahudson@gmail.com>
2025-05-31 22:19:38 +0100

    Use Webhooks

--- quiz_bot.py
+++ quiz_bot.py
@@ -1,17 +1,54 @@
 #!/usr/bin/env python3
-"""Create MCQs and Organize Quizzes"""
+"""Create MCQs on Telegram from JSON"""
+
 import asyncio
+from flask import Flask, Response, abort, make_response, request
+import html
+from http import HTTPStatus
+import logging
 import os
 import json
-from telegram import Bot, Poll, Update, MessageEntity, InputFile
-from telegram.ext import Application, CommandHandler, CallbackContext, MessageHandler, filters
+import uvicorn
+from asgiref.wsgi import WsgiToAsgi
+from telegram import Poll, Update, MessageEntity, InputFile
+from telegram.ext import (
+        Application,
+        CommandHandler,
+        CallbackContext,
+        MessageHandler,
+        filters,
+    )
 from telegram.constants import ParseMode
+from telegram.ext import Application
+from dotenv import load_dotenv
 
+
+# ========= LOGGING ==========
+logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO,
+)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
+
+
+# ========= CONFIG ===========
+load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
 if not TOKEN:
     raise ValueError("BOT_TOKEN environment variable is not set!")
 
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+if not WEBHOOK_URL:
+    raise ValueError("WEBHOOK_URL is not set!")
+
+WEBHOOK_PORT = int(os.getenv("WEBHOOK_PORT"))
+if not WEBHOOK_PORT:
+    raise ValueError("WEBHOOK_PORT is not set!")
+
+WEBHOOK_PATH = f"/{TOKEN}"
+SSL_CONTEXT = None
 
 DATA_FILE = "bot_data.json"
 
@@ -20,7 +57,7 @@ if not OWNER:
     raise ValueError("OWNER ID MUST BE SET")
 try:
     OWNER = int(OWNER)
-except:
+except Exception:
     raise ValueError("OWNER ID MUST BE A NUMBER")
 
 data = {"groups": [], "current_group": None}
@@ -28,13 +65,23 @@ if os.path.exists(DATA_FILE):
     with open(DATA_FILE, "r") as f:
         data = json.load(f)
 
+
+# =========== HELPER FUNCTIONS ===========
 def save_data():
     with open(DATA_FILE, "w") as f:
         json.dump(data, f, indent=2)
 
-async def start(update: Update, context: CallbackContext):
+
+async def verify_user(update: Update):
     if update.effective_user.id != OWNER:
-        await update.message.reply_text("Do not chat with me, I am a bot!")
+        await update.message.reply_text("You are not the boss of me!")
+        return False
+    return True
+
+
+# ============ HANDLERS ============
+async def start(update: Update, context: CallbackContext):
+    if await verify_user(update) is False:
         return
     message = (
         "*Welcome to AwesomeBro!*\n\n"
@@ -59,15 +106,13 @@ async def start(update: Update, context: CallbackContext):
 
 async def set_group(update: Update, context: CallbackContext):
     """Sets the group ID for the bot"""
-    if update.effective_user.id != OWNER:
-        await update.message.reply_text("Do not chat with me, I don\'t know you!")
+    if await verify_user(update) is False:
         return
-    try:
-        group_id = int(update.message.text.split()[-1])
-        group_id = str(group_id)
-    except ValueError:
-        await update.message.reply_text("Usage: /setgroup <group_id>")
-        return
+
+    group_id = update.message.text
+    print(group_id)
+    return
+
     if group_id not in data["groups"]:
         data["groups"].append(group_id)
     if data.get(group_id) is None or data[group_id].get("topics") is None:
@@ -79,12 +124,12 @@ async def set_group(update: Update, context: CallbackContext):
 
 async def add_topic(update: Update, context: CallbackContext):
     """Manually adds a topic"""
-    if update.effective_user.id != OWNER:
-        await update.message.reply_text("Do not chat with me, I don\'t know you!")
+    if await verify_user(update) is False:
         return
     parts = update.message.text.split(" ", 2)
     if len(parts) < 3:
-        await update.message.reply_text("Usage: /addtopic <topic_name> <topic_id>")
+        text = "Usage: /addtopic <topic_name> <topic_id>"
+        await update.message.reply_text(text)
         return
     topic_name = parts[1].strip().lower()
     try:
@@ -101,25 +146,25 @@ async def add_topic(update: Update, context: CallbackContext):
     with open(f"{current_group}_{topic_name}.json", 'w') as fp:
         json.dump([], fp)
         print(f"{topic_name}.json has been created!")
-    await update.message.reply_text(f"Topic '{topic_name}' added successfully!")
+    await update.message.reply_text(f"Topic '{topic_name}' added!")
 
 
 async def list_topics(update: Update, context: CallbackContext):
     """Lists all manually added topics."""
-    if update.effective_user.id != OWNER:
-        await update.message.reply_text("Do not chat with me, I am a bot!")
+    if await verify_user(update) is False:
         return
     if data["current_group"] is None:
         await update.message.reply_text("Set the current group first!!!")
         return
-    current_group = data["current_group"]
-    topics = "\n".join([f"{name}: {tid}" for name, tid in data[current_group]["topics"].items()])
-    await update.message.reply_text(f"Available topics:\n{topics if topics else 'No topics found.'}")
+    cg = data["current_group"]
+    topics = [f"{name}: {tid}" for name, tid in data[cg]["topics"].items()]
+    topics = "\n".join(topics)
+    await update.message.reply_text(f"Topics:\n{topics if topics else 'None'}")
+
 
 async def add_quiz(update: Update, context: CallbackContext):
     """Adds a quiz to a specific topic and stores it in a JSON file."""
-    if update.effective_user.id != OWNER:
-        await update.message.reply_text("Do not chat with me, I am a bot!")
+    if await verify_user(update) is False:
         return
     bot = context.bot
     if data["current_group"] is None:
@@ -127,15 +172,17 @@ async def add_quiz(update: Update, context: CallbackContext):
         return
     current_group = data["current_group"]
     parts = update.message.text.split("|")
-    
+
     if len(parts) < 4:
-        await update.message.reply_text("Usage: /addquiz topic | question | option1 | option2 | ... | correct_option_index")
+        text = "Usage: /addquiz topic | question | option1 | option2 \
+        | ... | correct_option_index"
+        await update.message.reply_text(text)
         return
 
     topic_name = parts[0].replace("/addquiz ", "").strip().lower()
     question = parts[1].strip()
     options = [opt.strip() for opt in parts[2:-1]]
-    
+
     try:
         correct_index = int(parts[-1].strip())
         if correct_index < 0 or correct_index >= len(options):
@@ -145,7 +192,8 @@ async def add_quiz(update: Update, context: CallbackContext):
         return
 
     if topic_name not in data[current_group]["topics"]:
-        await update.message.reply_text(f"Topic '{topic_name}' not found for {current_group}.")
+        text = f"Topic '{topic_name}' not found for {current_group}."
+        await update.message.reply_text(text)
         return
 
     topic_id = data[current_group]["topics"][topic_name]
@@ -175,13 +223,12 @@ async def add_quiz(update: Update, context: CallbackContext):
         message_thread_id=topic_id
     )
 
-    await update.message.reply_text(f"Quiz added to '{topic_name}' and stored successfully!")
+    await update.message.reply_text(f"Quiz added to {topic_name}!")
 
 
 async def bulk_add(update: Update, context: CallbackContext):
-    """Handles JSON file upload and adds quizzes in bulk, storing them in separate topic files."""
-    if update.effective_user.id != OWNER:
-        await update.message.reply_text("Do not chat with me, I am a bot!")
+    """Handles JSON file upload and adds quizzes in bulk"""
+    if await verify_user(update) is False:
         return
     if not update.message.document:
         await update.message.reply_text("Please upload a JSON file.")
@@ -225,7 +272,7 @@ async def bulk_add(update: Update, context: CallbackContext):
 
             topic_id = data[current_group]["topics"][topic_name]
             topic_file = f"{current_group}_{topic_name}.json"
-            
+
             await bot.send_poll(
                 chat_id=int(current_group),
                 question=question,
@@ -241,14 +288,13 @@ async def bulk_add(update: Update, context: CallbackContext):
             failed += 1
 
     summary = "Bulk upload summary:\n"
-    summary += "\n".join([f"{topic}: {count} added" for topic, count in added.items()])
+    summary += "\n".join([f"{t}: {c} added" for t, c in added.items()])
     summary += f"\nFailed: {failed}"
     await update.message.reply_text(summary)
 
 
 async def remove_topic(update: Update, context: CallbackContext):
-    if update.effective_user.id != OWNER:
-        await update.message.reply_text("Do not chat with me, I am a bot!")
+    if await verify_user(update) is False:
         return
     """Removes a topic from the bot."""
     if data["current_group"] is None:
@@ -264,21 +310,20 @@ async def remove_topic(update: Update, context: CallbackContext):
     if topic_name in data["topics"]:
         del data[current_group]["topics"][topic_name]
         save_data()
-        await update.message.reply_text(f"Topic '{topic_name}' removed from {current_group} successfully!")
+        await update.message.reply_text(f"Topic '{topic_name}' removed!")
     else:
-        await update.message.reply_text(f"Topic '{topic_name}' not found in {current_group}.")
+        await update.message.reply_text(f"Topic '{topic_name}' not found!")
 
 
 async def list_groups(update: Update, context: CallbackContext):
-    if update.effective_user.id != OWNER:
-        await update.message.reply_text("Do not chat with me, I don\'t know you!")
+    if await verify_user(update) is False:
         return
     groups_list = "\n".join(data["groups"])
     text = f"Available Groups:: \n {groups_list}"
     await update.message.reply_text(text)
 
 
-app = Application.builder().token(TOKEN).build()
+app = Application.builder().token(TOKEN).updater(None).build()
 app.add_handler(CommandHandler("removetopic", remove_topic))
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("setgroup", set_group))
@@ -289,6 +334,36 @@ app.add_handler(CommandHandler("listgroups", list_groups))
 app.add_handler(MessageHandler(filters.Document.ALL, bulk_add))
 
 
+flask_app = Flask(__name__)
+
+
+# ========== FLASK ROUTES ============
+@flask_app.post("/telegram")
+async def telegram():
+    await app.update_queue.put(Update.de_json(data=request.json, bot=app.bot))
+    return Response(status=HTTPStatus.OK)
+
+
+@flask_app.get("/healthcheck")
+async def healthcheck():
+    return make_response("✅ Bot is alive and kicking", HTTPStatus.OK)
+
+
+# =========== MAIN ==============
+async def main():
+    await app.bot.set_webhook(
+            url=f"{WEBHOOK_URL}/telegram",
+            allowed_updates=Update.ALL_TYPES
+        )
+    server = uvicorn.Server(
+            config=uvicorn.Config(app=WsgiToAsgi(flask_app),
+                                  host="0.0.0.0", port=WEBHOOK_PORT)
+        )
+
+    async with app:
+        await app.start()
+        await server.serve()
+        await app.stop()
+
 if __name__ == "__main__":
-    print("Bot is running...!")
-    app.run_polling()
+    asyncio.run(main())
